670f335b401f60b6aee3b0fe9c5b331725241f5a
BuNnYS814 <160164148+BuNnYS814@users.noreply.github.com>
2025-04-06 11:46:42 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -9,17 +9,24 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// MongoDB connection - using your new cluster
-mongoose.connect('mongodb+srv://pvsprasad814:pvsprasad8147@cluster0.ukgkdkw.mongodb.net/fastpay?retryWrites=true&w=majority')
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err));
+// Updated MongoDB connection with latest options
+mongoose.connect('mongodb+srv://pvsprasad814:pvsprasad8147@cluster0.ukgkdkw.mongodb.net/fastpay', {
+    serverSelectionTimeoutMS: 5000, // Timeout after 5s instead of 30s
+    socketTimeoutMS: 45000, // Close sockets after 45s of inactivity
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('MongoDB connection error:', err));
 
-// Routes
+// Import routes
 const userRoutes = require('./routes/userRoutes');
 const transactionRoutes = require('./routes/transactionRoutes');
+
+// Use routes
 app.use('/api', userRoutes);
 app.use('/api', transactionRoutes);
 
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
